b77fd67ac9f24d70afffaabca0b7d7ce9971c3a2
India Cornell <indiaccornell@gmail.com>
2023-11-19 15:48:27 -0800

    Update for API with promises and fetch

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,51 +2,91 @@ import React, { Component } from "react";
 import Loading from "./Loading";
 import Panel from "./Panel";
 import classnames from "classnames";
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads,
+} from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10,
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4,
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng",
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza",
-  },
+    getValue: getUserWithLeastUploads
+  }
+
 ];
 
 class Dashboard extends Component {
+  //initial state
   state = {
-    loading: false,
+    loading: true,
     focused: null,
+    photos: [],
+    topics: [],
   };
+  //lifecycle method - componentDidMount
+  componentDidMount() {
+    //fetch data from the API
+    const urlsPromise = [
+      "/api/photos",
+      "/api/topics",
+    ].map(url => fetch(url).then(res => res.json()));
+    //Promise.all() - takes an array of promises and returns a single promise
+    Promise.all(urlsPromise)
+    .then(([photos, topics]) => {
+      this.setState({
+        loading: false,
+        photos: photos,
+        topics: topics
+      });
+    });
+    //check if there is a focused item in localStorage
+    const focused = JSON.parse(localStorage.getItem("focused"));
+    if (focused) {
+      this.setState({focused});
+    }
+  }
+  //lifecycle method - componentDidUpdate
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
   //instance method
   selectPanel(id) {
-    this.setState(previousState => ({
-     focused: previousState.focused != null ? null : id
+    this.setState((previousState) => ({
+      focused: previousState.focused != null ? null : id,
     }));
-   }
-
+  }
+  //render method
   render() {
+    //classnames package - allows us to conditionally apply classes to an element
     const dashboardClasses = classnames("dashboard", {
-      "dashboard--focused": this.state.focused
-     });
-
+      "dashboard--focused": this.state.focused,
+    });
+    //if loading is true, return the Loading component
     if (this.state.loading) {
       return <Loading />;
     }
-
+    console.log(this.state); //test
+    //map over the data array and return a Panel component for each item in the array
     const panels = (
       this.state.focused
         ? data.filter((panel) => this.state.focused === panel.id)
@@ -55,9 +95,9 @@ class Dashboard extends Component {
       <Panel
         key={panel.id}
         label={panel.label}
-        value={panel.value}
+        value={panel.getValue(this.state)}
         //passing the instance method as a prop instead of above as a class property arrow method
-        onSelect={event => this.selectPanel(panel.id)}
+        onSelect={(event) => this.selectPanel(panel.id)}
       />
     ));
     return <main className={dashboardClasses}>{panels}</main>;
